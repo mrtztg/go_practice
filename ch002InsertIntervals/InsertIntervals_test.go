a9248d2cbbc2dfdbe15f69c1e25e3ae0636003c7
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-06-26 20:46:14 +0100

    add insertInterval testCases

--- ch002InsertIntervals/InsertIntervals_test.go
+++ ch002InsertIntervals/InsertIntervals_test.go
@@ -19,6 +19,10 @@ func TestSolutionInsertInterval(t *testing.T) {
 			[]Interval{{2, 3}, {5, 7}, {8, 12}},
 			Interval{4, 10},
 			[]Interval{{1, 3}, {4, 12}},
+		}, {
+			[]Interval{{2, 3}, {5, 7}},
+			Interval{1, 4},
+			[]Interval{{1, 4}, {5, 7}},
 		},
 	}
 	s := &solution{}
