4845daf12bfa4642751c80fc06ce12b3504571c4
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-07-10 19:16:48 +0100

    solve interval intersections

---
+++ ch003IntervalIntersection/IntervalIntersection.go
@@ -0,0 +1,48 @@
+package ch003IntervalIntersection
+
+type Interval struct {
+	Start, End int
+}
+
+type solution struct {
+}
+
+func (s *solution) merge(arr1, arr2 []Interval) []Interval {
+	i := 0
+	j := 0
+	merged := make([]Interval, 0)
+	for i < len(arr1) && j < len(arr2) {
+		intersectionInterval := Interval{
+			Start: max(arr1[i].Start, arr2[j].Start),
+			End:   min(arr1[i].End, arr2[j].End),
+		}
+		if intersectionInterval.End < intersectionInterval.Start {
+			if arr1[i].Start < arr2[j].Start {
+				i++
+			} else {
+				j++
+			}
+		} else {
+			merged = append(merged, intersectionInterval)
+			if arr1[i].End < arr2[j].End {
+				i++
+			} else {
+				j++
+			}
+		}
+	}
+	return merged
+}
+func min(val1, val2 int) int {
+	if val1 > val2 {
+		return val2
+	}
+	return val1
+}
+
+func max(val1, val2 int) int {
+	if val1 > val2 {
+		return val1
+	}
+	return val2
+}
