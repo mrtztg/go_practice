b3bc97daad9450d9f1adbfb56c8399f5309a83a3
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-07-10 17:09:34 +0100

    solve interst intervals

--- ch002InsertIntervals/InsertIntervals_test.go
+++ ch002InsertIntervals/InsertIntervals_test.go
@@ -14,9 +14,9 @@ func TestSolutionInsertInterval(t *testing.T) {
 		{
 			[]Interval{{1, 3}, {5, 7}, {8, 12}},
 			Interval{4, 6},
-			[]Interval{{1, 3}},
+			[]Interval{{1, 3}, {4, 7}, {8, 12}},
 		}, {
-			[]Interval{{2, 3}, {5, 7}, {8, 12}},
+			[]Interval{{1, 3}, {5, 7}, {8, 12}},
 			Interval{4, 10},
 			[]Interval{{1, 3}, {4, 12}},
 		}, {
