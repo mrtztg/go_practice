fb2eb9676de2ed992890142b9306e6c50c04e598
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-07-10 17:57:21 +0100

    improve insert interval solution

--- ch002InsertIntervals/InsertIntervals.go
+++ ch002InsertIntervals/InsertIntervals.go
@@ -7,27 +7,50 @@ type Interval struct {
 type solution struct {
 }
 
+//func (s *solution) insert(intervals []Interval, newInterval Interval) []Interval {
+//	newIntervalAdded := false
+//	merged := make([]Interval, 0)
+//	a := func(interv Interval) {
+//		if len(merged) == 0 || merged[len(merged)-1].End < interv.Start {
+//			merged = append(merged, interv)
+//		} else {
+//			merged[len(merged)-1].End = max(merged[len(merged)-1].End, interv.End)
+//		}
+//	}
+//	for _, interval := range intervals {
+//		if !newIntervalAdded {
+//			if newInterval.Start < interval.Start {
+//				a(newInterval)
+//				newIntervalAdded = true
+//			}
+//		}
+//		a(interval)
+//	}
+//	if !newIntervalAdded {
+//		a(newInterval)
+//	}
+//	return merged
+//}
+
 func (s *solution) insert(intervals []Interval, newInterval Interval) []Interval {
-	newIntervalAdded := false
+	if len(intervals) == 0 {
+		return []Interval{newInterval}
+	}
 	merged := make([]Interval, 0)
-	a := func(interv Interval) {
-		if len(merged) == 0 || merged[len(merged)-1].End < interv.Start {
-			merged = append(merged, interv)
-		} else {
-			merged[len(merged)-1].End = max(merged[len(merged)-1].End, interv.End)
-		}
+	i := 0
+	for i < len(intervals) && intervals[i].End < newInterval.Start {
+		merged = append(merged, intervals[i])
+		i++
 	}
-	for _, interval := range intervals {
-		if !newIntervalAdded {
-			if newInterval.Start < interval.Start {
-				a(newInterval)
-				newIntervalAdded = true
-			}
-		}
-		a(interval)
+	for i < len(intervals) && newInterval.End >= intervals[i].Start {
+		newInterval.Start = min(newInterval.Start, intervals[i].Start)
+		newInterval.End = max(newInterval.End, intervals[i].End)
+		i++
 	}
-	if !newIntervalAdded {
-		a(newInterval)
+	merged = append(merged, newInterval)
+	for i < len(intervals) {
+		merged = append(merged, intervals[i])
+		i++
 	}
 	return merged
 }
