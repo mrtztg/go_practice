8781bb306512ac624819e04c036f904e20d6a28c
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-07-11 14:39:51 +0100

    solve cyclic sort

--- ch005CyclicSort/cyclicSort.go
+++ ch005CyclicSort/cyclicSort.go
@@ -2,6 +2,16 @@ package ch005CyclicSort
 
 type Solution struct{}
 
-func (s *Solution) sort(nums []int) []int {
-	return nums
+func (s *Solution) sort(Nums []int) []int {
+	if len(Nums) < 2 {
+		return Nums
+	}
+	for i := 0; i < len(Nums); i++ {
+		for Nums[i] != i+1 {
+			tempNo := Nums[i]
+			Nums[i] = Nums[tempNo-1]
+			Nums[tempNo-1] = tempNo
+		}
+	}
+	return Nums
 }
