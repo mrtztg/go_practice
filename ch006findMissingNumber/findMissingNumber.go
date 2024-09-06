0f63cd2bb44ace52fe165f6912c861c481909287
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-09-06 20:06:02 +0100

    solve findMissingNumber

--- ch006findMissingNumber/findMissingNumber.go
+++ ch006findMissingNumber/findMissingNumber.go
@@ -4,5 +4,20 @@ type Solution struct {
 }
 
 func (s *Solution) findMissingNumber(nums []int) int {
-
+	i := 0
+	for i < len(nums) {
+		if i != nums[i] && nums[i] != len(nums) {
+			tempNumber := nums[i]
+			nums[i] = nums[tempNumber]
+			nums[tempNumber] = tempNumber
+		} else {
+			i++
+		}
+	}
+	for i = 0; i < len(nums); i++ {
+		if nums[i] != i {
+			return i
+		}
+	}
+	return len(nums)
 }
