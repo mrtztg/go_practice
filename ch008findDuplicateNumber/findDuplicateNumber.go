146f3bdebf3f5fed6bf48051d364d990a8ddc096
Morteza <76879732+mrtztg@users.noreply.github.com>
2024-09-16 19:49:52 +0100

    solve findDuplicateNumber

--- ch008findDuplicateNumber/findDuplicateNumber.go
+++ ch008findDuplicateNumber/findDuplicateNumber.go
@@ -1,5 +1,14 @@
 package ch008findduplicatenumber
 
 func findDuplicateNumber(nums []int) int{
-	
+	for i := 0; i < len(nums); {
+		if nums[i] != nums[nums[i] - 1] {
+			nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
+		} else if i + 1 != nums[i] {
+			return nums[i]
+		} else {
+			i++
+		}
+	}
+	return -1
 }
